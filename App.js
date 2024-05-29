9561ae16578885ea8d3e64c3a8181b462bacc4e7
mengva chuepor <you@example.com>
2024-05-29 01:55:07 -0500

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
+// App.js
+
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import Counter from "./Counter";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./components/Home";
+import Details from "./components/Details";
+import Profile from "./components/Profile";
+import Setting from "./components/Setting";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Counter initialValue={0} />
-    </View>
-  );
+
+return (
+  <>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+  </NavigationContainer>
+
+
+  <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Setting" component={Setting} />
+    </Tab.Navigator>
+  </NavigationContainer>
+  </>
+);
+
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "lightblue",
-  },
-});
